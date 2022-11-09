115d3cab4a64c156dd03a53c55f5140948e6e6f9
muntasir <sajeebmuntasir0@gmail.com>
2022-11-09 18:49:03 +0600

    service by id api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -36,6 +36,38 @@ app.get('/services/home', async (req, res) => {
         res.send({
             success: true,
             message: "successful",
+            data: result
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+//services 
+app.get("/services", async (req, res) => {
+    try {
+        const result = await Services.find({}).toArray();
+        res.send({
+            success: true,
+            data: result
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+//services by id
+app.get('/services/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await Services.findOne({ _id: ObjectId(id) })
+        res.send({
+            success: true,
             data: result
         })
     } catch (error) {
